81902177a805fbce7a0074a24fce38127c44879d
Yunyang-syy <simonasunyunyang@outlook.com>
2025-03-29 01:24:04 -0400

    course pages skeleton completed

---
+++ app/courses/[courseId]/materials/page.tsx
@@ -0,0 +1,8 @@
+export default function CourseMaterialsPage() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Course Materials</h1>
+        {/* Content goes here */}
+      </div>
+    );
+  }
